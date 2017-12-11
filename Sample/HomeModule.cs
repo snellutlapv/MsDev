698cf7e9d8b9709c83968d6c9243a53e9a6e9977
snellutlapv <snellutla@practicevelocity.com>
2017-12-11 12:05:20 -0600

    test

---
+++ Sample/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Sample
+{
+    public class HomeModule:NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World";
+        }
+    }
+}
