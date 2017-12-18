85592fe3c0439cadb8d1756b33a77da93d4ca652
snellutlapv <snellutlapv@github.com>
2017-12-18 08:43:13 -0600

    Updated Nancy Utils

---
+++ NancyUtilities/Cryptography.cs
@@ -0,0 +1,22 @@
+﻿using Nancy.Cryptography;
+
+namespace NancyUtilities
+{
+    public class Cryptography : IEncryptionProvider
+    {
+        private static CryptographyConfiguration cryptographyConfiguration
+        => new CryptographyConfiguration(
+                    new RijndaelEncryptionProvider(new PassphraseKeyGenerator("SuperSecretPass", new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 })),
+                    new DefaultHmacProvider(new PassphraseKeyGenerator("UberSuperSecure", new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 })));
+
+        public string Encrypt(string data)
+        {
+            return cryptographyConfiguration.EncryptionProvider.Encrypt(data);
+        }
+
+        public string Decrypt(string data)
+        {
+            return cryptographyConfiguration.EncryptionProvider.Decrypt(data);           
+        }
+    }
+}
---
+++ NancyUtilities/LogServerInstance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace NancyUtilities
+{
+    public class LogServerInstance
+    {
+        public static async Task UpdateDbWithServerInfo(string connString, string spName, string hostUri, string port, string serviceName)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (var tran = conn.BeginTransaction())
+                {
+                    using (var cmd = new SqlCommand(spName, conn, tran))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add(new SqlParameter("@ServiceName", serviceName));
+                        cmd.Parameters.Add(new SqlParameter("@ServiceUri", hostUri));
+                        cmd.Parameters.Add(new SqlParameter("@Port", port));
+                        try
+                        {
+                            await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                        }
+                        catch (Exception)
+                        {
+                            tran.Rollback();
+                        }
+                        tran.Commit();
+                    }
+                }
+            }
+        }
+    }
+}
