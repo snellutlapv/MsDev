dab7e12840f9fef2e329ee307c78e428ff800d86
snellutlapv <snellutla@practicevelocity.com>
2017-12-20 10:34:00 -0600

    updated nancyutules

--- NancyUtilities/LogServerInstance.cs
+++ NancyUtilities/LogServerInstance.cs
@@ -33,5 +33,39 @@ namespace NancyUtilities
                 }
             }
         }
+
+        public static async Task UpdateDbWithServerLog(string connString, string serviceUrl, string serviceName,
+            string methodName, string actionName, string referrer, string query, string responseTime)
+        {
+            var sqlSpName = "shms.dbo.[CreateServiceLog]";
+
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (var tran = conn.BeginTransaction())
+                {
+                    using (var cmd = new SqlCommand(sqlSpName, conn, tran))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add(new SqlParameter("@ServiceUrl", serviceUrl));
+                        cmd.Parameters.Add(new SqlParameter("@ResponseTime", responseTime));
+                        cmd.Parameters.Add(new SqlParameter("@ServiceName", serviceName));
+                        cmd.Parameters.Add(new SqlParameter("@MethodName", methodName));
+                        cmd.Parameters.Add(new SqlParameter("@Action", actionName));
+                        cmd.Parameters.Add(new SqlParameter("@Referrer", referrer));
+                        cmd.Parameters.Add(new SqlParameter("@Query", query));
+                        try
+                        {
+                            await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+                        }
+                        catch (Exception)
+                        {
+                            tran.Rollback();
+                        }
+                        tran.Commit();
+                    }
+                }
+            }
+        }
     }
 }
